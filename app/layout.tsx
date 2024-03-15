63cfbfee5f765932b547fa478c675d103e48e308
Suman Mondal <mondalsuman97322@gmail.com>
2024-03-16 03:57:10 +0530

    Full Project Completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { ConvexClientProvider } from "@/providers/convex-client-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { ModalProvider } from "@/providers/modal-provider";
 import { Analytics } from "@vercel/analytics/react";
+import { Suspense } from "react";
+import { Loading } from "@/components/auth/loading";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -24,12 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <ConvexClientProvider>
-          <Toaster />
-          <ModalProvider />
-          {children}
-          <Analytics />
-        </ConvexClientProvider>
+        {/* <Suspense fallback={<Loading />}> */}
+          <ConvexClientProvider>
+            <Toaster />
+            <ModalProvider />
+            {children}
+            <Analytics />
+          </ConvexClientProvider>
+        {/* </Suspense> */}
       </body>
     </html>
   );
